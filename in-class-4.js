681a633756faeb2119f61920e4bfcbbd28660f0a
njahangir23 <njahangir@fordham.edu>
2024-09-25 17:08:11 -0400

    Update in-class-4.js

--- in-class-4.js
+++ in-class-4.js
@@ -6,7 +6,7 @@ for(let i=0; i<array.length; i++)
 {
    if(array[i]==1 || array[i]==3 || array[i]==5)
     {
-        console.log(`${array[i]} is odd`);
+        console.log(`${array[i]} is odd!`);
         }
     else if(array[i]==2 || array[i]==4)
     {
